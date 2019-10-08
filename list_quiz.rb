874edc25e16640ab7a2d7d4811f5c3c35ff921ba
Brennan Baker <bakerbj26@gmail.com>
2019-10-08 08:54:28 -0600

    solved number 2

---
+++ list_quiz.rb
@@ -0,0 +1,42 @@
+# def threeven(list)
+#     list.each do |n|
+#         if n % 2 == 0 && list[n + 1] % 2 == 0 && list[n + 2] % 2 == 0
+#             return true
+#         end
+#     end
+#     return false
+
+# end
+
+
+# puts threeven([2, 1, 3, 5])
+# puts threeven([2, 4, 12, 5])
+# puts threeven([2, 1, 4, 6])
+# puts threeven([1, 4 ,6, 4])
+#puts threeven([])
+
+
+
+
+
+
+def bigger_two(list1, list2)
+    sum1 = 0
+    sum2 = 0
+    list1.each do |n|
+        sum1 = sum1 + n
+    end
+    list2.each do |i|
+        sum2 = sum2 + i
+    end
+    if sum1 >= sum2
+        print list1
+    else
+        print list2
+    end
+
+
+end
+
+puts bigger_two([1, 2], [3, 4])
+puts bigger_two([1, 7], [4, 4])
